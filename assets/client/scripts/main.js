b9d87c9462099d0c9a111b993e85cb2206f6e68c
Kl1menko <klimenko.vitaliy2016@icloud.com>
2025-07-31 23:03:00 -0700

    add block opportunities, about-us

--- assets/client/scripts/main.js
+++ assets/client/scripts/main.js
@@ -1,22 +1,22 @@
-import Header from './Header.js'
-import TabsCollection from './Tabs.js'
-import VideoPlayerCollection from './VideoPlayer.js'
-import ExpandableContentCollection from './ExpandableContent.js'
-import InputMaskCollection from './InputMask.js'
-import SelectCollection from './Select.js'
-import defineScrollBarWidthCSSVar from './utils/defineScrollBarWidthCSSVar.js'
+import Header from "./Header.js";
+import TabsCollection from "./Tabs.js";
+import VideoPlayerCollection from "./VideoPlayer.js";
+import ExpandableContentCollection from "./ExpandableContent.js";
+import InputMaskCollection from "./InputMask.js";
+import SelectCollection from "./Select.js";
+import defineScrollBarWidthCSSVar from "./utils/defineScrollBarWidthCSSVar.js";
+import TextAnimation from "./TextAnimation.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
+  setTimeout(() => {
+    new Header();
+    new TabsCollection();
+    new VideoPlayerCollection();
+    new ExpandableContentCollection();
+    new InputMaskCollection();
+    new SelectCollection();
+    new TextAnimation("[data-animation-text]").init();
 
-    setTimeout(() => {
-        new Header()
-        new TabsCollection()
-        new VideoPlayerCollection()
-        new ExpandableContentCollection()
-        new InputMaskCollection()
-        new SelectCollection()
-
-        defineScrollBarWidthCSSVar()
-    }, 1000); // Delay to ensure all elements are loaded
-
+    defineScrollBarWidthCSSVar();
+  }, 1000); // Delay to ensure all elements are loaded
 });

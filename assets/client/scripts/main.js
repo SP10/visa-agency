2c517dde0b56d40dd48b8adecdca10e5541269f9
Serhii Pavlovych <serhiipavlovych@Mac.localdomain>
2025-08-02 06:25:43 +0300

    added carousel for countries

--- assets/client/scripts/main.js
+++ assets/client/scripts/main.js
@@ -6,6 +6,7 @@ import InputMaskCollection from "./InputMask.js";
 import SelectCollection from "./Select.js";
 import defineScrollBarWidthCSSVar from "./utils/defineScrollBarWidthCSSVar.js";
 import TextAnimation from "./TextAnimation.js";
+import Carousel from "./Сarousel.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   setTimeout(() => {
@@ -16,6 +17,40 @@ document.addEventListener("DOMContentLoaded", async () => {
     new InputMaskCollection();
     new SelectCollection();
     new TextAnimation("[data-animation-text]").init();
+    new Carousel('[data-carousel]', {
+      slidesPerView: 1,
+      navigation: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: true,
+      paginationClickable: true,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        }
+      },
+      injectStyles: [
+        `
+      .swiper-pagination {
+          position: static;
+          margin-top: 32px;
+      }
+      .swiper-pagination-bullet-active {
+          width: 14px;
+          height: 14px;
+          vertical-align: middle;
+      }
+      `,
+      ],
+      on: {
+        init() {
+          // ...
+        },
+      },
+    });
+
 
     defineScrollBarWidthCSSVar();
   }, 1000); // Delay to ensure all elements are loaded
--- assets/client/scripts/Сarousel.js
+++ assets/client/scripts/Сarousel.js
@@ -1,52 +1,24 @@
-window.onload = (event) => {
-    // swiper element
-    const swiperElements = document.querySelectorAll('swiper-container');
-
-    // swiper parameters
-    const swiperParams = {
-        slidesPerView: 1.2,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: true,
-        paginationClickable: true,
-        breakpoints: {
-            768: {
-              slidesPerView: 2,
-            }
-        },
-        injectStyles: [
-        `
-        .swiper-pagination {
-            position: static;
-            margin-top: 32px;
-        }
-        .swiper-pagination-bullet-active {
-            width: 14px;
-            height: 14px;
-            vertical-align: middle;
-        }
-        `,
-        ],
-        on: {
-        init() {
-            // ...
-        },
-        },
-    };
-
-    for(i = 0; i < swiperElements.length; i++) {
-
-        const slidesPerView = swiperElements[i].getAttribute('data-slides-per-view');
-        if (slidesPerView) {
-            swiperParams.breakpoints[768].slidesPerView = slidesPerView;
-        }
-        // now we need to assign all parameters to Swiper element
-        Object.assign(swiperElements[i], swiperParams);
-
-        // and now initialize it
-        swiperElements[i].initialize();
+class Carousel {
+    constructor(selector, config = {}) {
+        this.selector = selector || '[data-carousel]';
+        this.config = config;
+        this.init();
     }
 
-};
+    init() {
+        let element = document.querySelectorAll(this.selector);
+        element.forEach((el) => {
+            const slidesPerView =  el.getAttribute('data-slides-per-view');
+            if (slidesPerView) {
+                this.config.breakpoints[768].slidesPerView = slidesPerView;
+            }
+            // now we need to assign all parameters to Swiper element
+            Object.assign(el,  this.config);
+
+            // and now initialize it
+            el.initialize();
+        });
+    }
+}
+
+export default Carousel
